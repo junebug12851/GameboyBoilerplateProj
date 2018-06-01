2315212a0a73fc03791bc08705abc7e0743876fa
June Hanabi <junehanabi@gmail.com>
2018-06-01 11:01:57 -0400

    Overhaul on joypad code

--- src/Home/Joypad.asm
+++ src/Home/Joypad.asm
@@ -4,26 +4,14 @@ include "./src/Setup/Hardware.inc"
 section "JoypadMngmt", rom0
 
 ReadJoypad::
-	ld  a,JOYF_DPAD ;select dpad
-	JOY_DEBOUNCE
-	cpl 			 ;complement a (Invert)
-	and JOYF_MASK_READ   ;Keep dpad buttons
-	swap a
+	; Read all joypad buttons
+	joypad_columns
 
-	ld  b,a ; Load DPAD Buttons only on upper half
+	ld b, a ; Backup
 
-	ld  a,JOYF_BTNS ;select btns
-	JOY_DEBOUNCE
-	cpl 			 ;complement a (Invert)
-	and JOYF_MASK_READ   ;Keep dpad buttons
+	; Save joypad buttons both down and up into memory
+	joypad_save
 
-	or  b ; Merge dpad buttons with other buttons
-
-	ld  b,a
-	ld  a,[wJoyDown]
-	cpl
-	and b
-	ld  [wJoyUp],a ; Save which buttons were pressed and then released
-	ld  a,b
-	ld  [wJoyDown],a ; Save which buttons are being pressed
+	; Reset joypad
+	joypad_reset
 	ret
